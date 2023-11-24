5e83325da811cbd8d6d7260f02d5491acef4143f
Blunde1 <lundeberent@gmail.com>
2023-11-24 13:47:11 +0100

    EnIF class and structure High-level API: - fit - transport Low-level API: - fit_precision - fit_H - pushforward_to_canonical - update_canonical - pullback_from_canonical

---
+++ graphite_maps/enif.py
@@ -0,0 +1,77 @@
+from typing import Optional
+import numpy as np
+from scipy.sparse import spmatrix  # type: ignore
+import networkx as nx  # type: ignore
+
+
+class EnIF:
+    def __init__(
+        self,
+        Prec_u: Optional[spmatrix],
+        Graph_u: Optional[nx.Graph],
+        Prec_eps: Optional[spmatrix],
+        H: Optional[spmatrix] = None,
+    ) -> None:
+        assert (
+            Prec_u is not None or Graph_u is not None
+        ), "Provide either Prec_u or Graph_u"
+
+        # Initialize your attributes here
+        self.Prec_u = Prec_u
+        self.Graph_u = Graph_u
+        self.Prec_eps = Prec_eps
+        self.H = H
+
+    def fit(
+        self,
+        U: np.ndarray,
+        Y: Optional[np.ndarray] = None,
+        verbose: bool = True,
+    ) -> None:
+        """
+        Fit precision of u and (sparse) mapping H:u->y.
+        """
+        if self.Prec_u is None:
+            self.fit_precision(U)
+        elif verbose:
+            print("Precision u exists. Use `fit_precision` to refit")
+        if self.H is None:
+            assert (
+                Y is not None
+            ), "Y = h(U) response must be provided if H is not passed on init"
+            self.H = self.fit_H(U, Y)
+        elif verbose:
+            print("H mapping exists. Use `fit_H` to refit")
+
+    def transport(self, U: np.ndarray, d: np.ndarray) -> np.ndarray:
+        """
+        Transport U from a sample from the prior to the posterior
+        """
+        canonical = self.pushforward_to_canonical(U)
+        updated = self.update_canonical(canonical, d)
+        return self.pullback_from_canonical(updated)
+
+    # Low-level API methods
+    def fit_precision(self, U: np.ndarray) -> None:
+        # Implement fitting precision matrix logic here
+        pass
+
+    def fit_H(self, U: np.ndarray, Y: np.ndarray) -> np.ndarray:
+        # Placeholder implementation to satisfy type-hinting
+        H_fitted = np.array([])  # Empty array as a placeholder
+        # TODO: Replace with actual LASSO logic
+        return H_fitted
+
+    def pushforward_to_canonical(self, U: np.ndarray) -> np.ndarray:
+        # TODO: Replace with actual pushforward logic
+        return np.array([])
+
+    def update_canonical(
+        self, canonical: np.ndarray, d: np.ndarray
+    ) -> np.ndarray:
+        # TODO: Replace with actual update canonical logic
+        return np.array([])
+
+    def pullback_from_canonical(self, updated: np.ndarray) -> np.ndarray:
+        # TODO: Replace with actual pollback logic
+        return np.array([])
