568aeb4bef0bd7c0bcc9edb7dfc1b413ca1e18a4
Yngve S. Kristiansen <ynk@equinor.com>
2025-04-02 13:42:38 +0200

    Add build, test, publish for linux

---
+++ wheel_build_scripts/build_wheels_linux.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+
+if [ -z "$1" ]; then
+  echo "Please provide a Python version as an argument (e.g., 3.10)"
+  exit 1
+fi
+
+INSTALL_DIR=/github/workspace/deps_build
+
+pushd /tmp
+
+python_exec=$(which python$1)
+$python_exec -m venv myvenv
+source ./myvenv/bin/activate
+
+yum install -y suitesparse-devel
+python -m pip install --upgrade pip
+pip install --upgrade setuptools wheel auditwheel
+
+git clone --branch v0.4.15 --single-branch https://github.com/scikit-sparse/scikit-sparse
+LD_LIBRARY_PATH=/usr/lib64:$LD_LIBRARY_PATH pip install --editable scikit-sparse
+
+popd
+echo "copying sksparse into graphite_maps/sksparse"
+cp -r /tmp/scikit-sparse/sksparse graphite_maps/sksparse
+
+# Breaks the install, fixes the wheel
+echo "adding wheel-only setup.py..."
+mv wheel_build_scripts/setup_wheels_only.py setup.py
+
+echo "Making wheel..."
+LD_LIBRARY_PATH=/usr/lib64:$LD_LIBRARY_PATH pip wheel .
+echo "Done making wheel"
+
+pip install auditwheel
+wheel_path=$(find . -name "graphite*.whl")
+auditwheel repair $wheel_path -w fixed_wheel
+fixed_wheel_path=$(find fixed_wheel -name "*.whl")
+
+echo "Replacing broken wheel @ $wheel_path with fixed wheel @ $fixed_wheel_path"
+rm $wheel_path
+mv $fixed_wheel_path $wheel_path
