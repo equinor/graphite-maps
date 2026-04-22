3665b11c70201c989d2d807b18e83bf13398acb1
tommyod <tommy.odland>
2026-04-22 06:41:52 +0200

    Test that two objective calls yield same result, as well as numerical gradient

--- tests/test_precision_estimation.py
+++ tests/test_precision_estimation.py
@@ -1,7 +1,30 @@
 import numpy as np
+import scipy as sp
 from graphite_maps import precision_estimation as precest
 from scipy.linalg import det
 from scipy.sparse import diags
+
+
+def test_objective_twice():
+    # A regression test: ensure that two calls return the same result.
+    rng = np.random.default_rng(42)
+
+    C_k = np.exp(rng.normal(0, 0.1, size=5))
+    U = rng.normal(size=(5, 5))
+
+    value1 = precest.objective_function(C_k, U)
+    value2 = precest.objective_function(C_k, U)
+    np.testing.assert_allclose(value1, value2)
+
+    # Check gradient
+    rmse = sp.optimize.check_grad(
+        precest.objective_function,
+        precest.gradient,
+        np.array([1, 2, 3, 4, 4.5]),
+        U,
+        rng=rng,
+    )
+    assert rmse <= 0.002
 
 
 def test_precision_graph_conversion():
